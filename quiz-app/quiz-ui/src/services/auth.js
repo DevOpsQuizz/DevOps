fd3804d91d0ac995946f63ff967b654cd95669f1
Nassim <nassim.mahnane@edu.esiee.fr>
2025-12-10 21:51:22 +0100

    UI yml test

--- quiz-app/quiz-ui/src/services/auth.js
+++ quiz-app/quiz-ui/src/services/auth.js
@@ -1,5 +1,6 @@
 const API_BASE = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000'
 
+// Admin login
 export async function loginAdmin(password) {
   const url = `${API_BASE}/login/`
   console.log('POST ->', url)
